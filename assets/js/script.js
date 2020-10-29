786bb6408904d6587bbc1427e5f47c14d7738b3d
Karl-Johan Westhoff <karl.johan.westhoff@gmail.com>
2020-10-29 15:14:11 -0700

    readme updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,23 @@
 console.log("is it working..")
 
+var key = "e802f453e0d44b4a8cf3f06882eee4f9";
+
+var keyname = "Ocp-Apim-Subscription-Key";
+
+init = {
+    "method":"GET",
+    "headers" : {"Ocp-Apim-Subscription-Key":"e802f453e0d44b4a8cf3f06882eee4f9"},
+};
+
+url = `https://gateway.apiportal.ns.nl/reisinformatie-api/api/v2/disruptions`;
+
+
+fetch(url, init).then(function(res){
+    if(!res.ok){
+        console.log("its not working");        
+    };    
+    console.log("response code" + res.status);
+    return res.json();
+}).then(function(data){
+    console.log(data);
+});
