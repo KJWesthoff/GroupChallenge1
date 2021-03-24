a4c119808c9cae92f611a146687a6354b5890d9b
Karl-Johan Westhoff <karl.johan.westhoff@gmail.com>
2021-03-23 18:59:30 -0700

    set up basic express server

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
+const express = require('express');
+var path = require('path');
 
 
-// app.js
+// server configuration
+const PORT = process.env.PORT || 3000;
+const app = express();
 
-const http = require('http');
+app.use(express.static(__dirname));
 
-// Create an instance of the http server to handle HTTP requests
-let app = http.createServer((req, res) => {
-    // Set a response type of plain text for the response
-    res.writeHead(200, {'Content-Type': 'http'});
-
-    
-    // Send back a response and end the connection
-    res.end("/test");
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-// Start the server on port 3000
-app.listen(3000, '127.0.0.1');
-console.log('Node server running on port 3000');
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`)
+  });
